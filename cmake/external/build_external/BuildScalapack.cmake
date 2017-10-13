9c36a584e3909548b7b3ffaad0133ffdd33cf002
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-10-13 15:04:45 -0700

    update build scripts for blas/lapack
    Former-commit-id: a9b264442e287cf9cc9a2083c0ceb7e9449d15e3

--- cmake/external/build_external/BuildScalapack.cmake
+++ cmake/external/build_external/BuildScalapack.cmake
@@ -1,19 +1,21 @@
 
 include(ExternalProject)
 
-#Keep it simple for now, build if user does not provide the following cmake variables
-if (SCALAPACK_LIBRARY_PATH AND SCALAPACK_LIBRARIES)
+if (SCALAPACK_LIBRARIES)
     add_library(scalapack${TARGET_SUFFIX} INTERFACE)
-else()
+elseif(BUILD_NETLIB_BLAS_LAPACK)
     set(SCALAPACK_ROOT_DIR ${CMAKE_INSTALL_PREFIX}/scalapack)
     message(STATUS "Building SCALAPACK at: ${SCALAPACK_ROOT_DIR}")
 
     ExternalProject_Add(scalapack${TARGET_SUFFIX}
         URL http://www.netlib.org/scalapack/scalapack.tgz
-        CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TESTING=OFF -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TESTING=OFF 
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
         -DCMAKE_INSTALL_PREFIX=${SCALAPACK_ROOT_DIR}
-        INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
+        INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install DESTDIR=${STAGE_DIR}        
     )
+else()
+    message(ERROR "Please provide the SCALAPACK_LIBRARIES cmake variable or 
+    set BUILD_NETLIB_BLAS_LAPACK to ON if you the NWChemEx build to install Netlib SCALAPACK.")
 
 endif()
