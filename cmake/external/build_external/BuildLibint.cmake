1586dd2143148a79806ccfa87cbed04204a5aad1
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-10-14 13:20:08 -0700

    Build libint 2.4 which has some bug fixes
    Former-commit-id: aa64405e0900d769c8c28dfa226a526713d4682b

--- cmake/external/build_external/BuildLibint.cmake
+++ cmake/external/build_external/BuildLibint.cmake
@@ -1,7 +1,7 @@
 set(LIBINT_FLAGS ${CMAKE_CXX_FLAGS} -fPIC)
 
 ExternalProject_Add(Libint${TARGET_SUFFIX}
-    URL https://github.com/evaleev/libint/releases/download/v2.3.1/libint-2.3.1.tgz
+    URL https://github.com/evaleev/libint/releases/download/v2.4.0-beta.4/libint-2.4.0-beta.4.tgz
     CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}
         CXX=${CMAKE_CXX_COMPILER}
         CC=${CMAKE_C_COMPILER}
