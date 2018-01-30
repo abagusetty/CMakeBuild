130956af0113f9d2c4482a300d24057b86c3a7e0
Ryan Richard <ryanmrichard1@gmail.com>
2018-01-30 11:51:24 -0600

    fixes a bug with same name headers (and updates macro documentation) (#27)
    * fixes a bug with same name headers (and updates macro documentation)
    
    * now the bug's fixed...also toggles automatic verbose Makefiles when cmake is in debug mode
    
    
    Former-commit-id: 706e1e47c8420401504bbe0a49fd4dd22df02b7b

--- cmake/macros/DependencyMacros.cmake
+++ cmake/macros/DependencyMacros.cmake
@@ -8,23 +8,6 @@ include(DebuggingMacros)
 include(UtilityMacros)
 include(AssertMacros)
 
-#
-# Macro for finding a dependency.  It is assumed that whatever name is given
-# to this macro, is a name that can be directly fed to CMake's find_package
-# macro.  Furthermore it is assumed that the result is given back as the
-# standard variables, e.g.
-# XXX_INCLUDE_DIRS, XXX_LIBRARIES, etc. where XXX is the name of the library in
-# all capital letters.  If the library is found the variables passed to
-# this function will be set appropriately
-#
-# Syntax: find_dependency(<name> <include> <lib> <flags> <lflags> <found>)
-#    - name      : The name to be passed to find_package
-#    - include   : The variable to append the includes on to
-#    - lib       : The variable to append the libraries on to
-#    - flags     : The variable to append the compile-time flags on to
-#    - lflags    : Same as flags except for linking
-#    - found " True if we actually found the requested dependency
-#
 function(find_dependency __name _include_dirs _libraries _definitions
                                 _link_flags _found)
     #This will be messy for packages relying on Config files if we haven't built
@@ -57,13 +40,7 @@ function(find_dependency __name _include_dirs _libraries _definitions
     endif()
 endfunction()
 
-#
-# Macro for finding a dependency and building it if it is not found.  Either way
-# a target with the name of the dependency suffixed with "_External" will be
-# added.
-#    - name : The case-sensitive name for the dependency
-#    - was_found : a variable whose value will be true if name was found
-#
+
 function(find_or_build_dependency __name __was_found)
     if(TARGET ${__name}_External)
         debug_message("${__name} already handled.")
