a4dc287d978f1f527197f5d6ac2ec3861e12d386
Ajay Panyala <ajay.panyala@gmail.com>
2018-03-19 12:22:03 -0700

    fixes to FindGlobalArrays.cmake (#56)
    * Fixes to FindGlobalArrays.cmake to find pthreads and librt
    
    * Minor
    
    * minor fix
    
    * Fix GA configure issues with blas, lapack. Use ga-config to get ga + dependent libs
    
    
    Former-commit-id: 1200eacd48252fa7a063597f1667e172a07f23b9

--- cmake/find_external/FindGlobalArrays.cmake
+++ cmake/find_external/FindGlobalArrays.cmake
@@ -14,6 +14,9 @@ if(NOT DEFINED GLOBALARRAYS_ROOT_DIR)
     pkg_check_modules(PC_GLOBALARRAYS QUIET ga)
 endif()
 
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+find_package(Threads REQUIRED)
+
 find_path(GLOBALARRAYS_INCLUDE_DIR ga.h
           HINTS ${PC_GLOBALARRAYS_INCLUDEDIR}
                 ${PC_GLOBALARRAYS_INCLUDE_DIRS}
@@ -38,8 +41,15 @@ find_library(GLOBALARRAYS_ARMCI_LIBRARY libarmci${CMAKE_STATIC_LIBRARY_SUFFIX}
                    ${PC_GLOBALARRAYS_LIBRARY_DIRS}
              PATHS ${GLOBALARRAYS_ROOT_DIR}
         )
+
 set(GLOBALARRAYS_LIBRARIES ${GLOBALARRAYS_C_LIBRARY}
-                           ${GLOBALARRAYS_ARMCI_LIBRARY})
+                           ${GLOBALARRAYS_ARMCI_LIBRARY} 
+                           ${CMAKE_THREAD_LIBS_INIT})
+
+find_package(LibRT)
+if(LIBRT_LIBRARIES)
+  set(GLOBALARRAYS_LIBRARIES ${GLOBALARRAYS_LIBRARIES} ${LIBRT_LIBRARIES})
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GlobalArrays DEFAULT_MSG
@@ -49,4 +59,16 @@ find_package_handle_standard_args(GlobalArrays DEFAULT_MSG
 )
 set(GLOBALARRAYS_INCLUDE_DIRS ${GLOBALARRAYS_INCLUDE_DIR})
 set(GLOBALARRAYS_FOUND ${GlobalArrays_FOUND})
+
+#Get GA + dependent libs using ga-config script
+#Usually the dependent libs are MPI, blas, lapack, pthreads, librt
+#The ga-config script does not need to verify that these dependency libs actually exist since
+#that is already done by the corresponding FindPackage module and passed to GA configure in BuildGlobalArrays.cmake
+execute_process(COMMAND ${GLOBALARRAYS_CONFIG}/ga-config --libs OUTPUT_VARIABLE GA_CONFIG_LIBS OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND ${GLOBALARRAYS_CONFIG}/ga-config --ldflags OUTPUT_VARIABLE GA_CONFIG_LDFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+#Linker options for Threads & librt will be provided through ga-config in the next release (5.6.4) of GA
+set(GLOBALARRAYS_LIBRARIES ${GLOBALARRAYS_LIBRARIES} ${GA_CONFIG_LDFLAGS} ${GA_CONFIG_LIBS})
+
+
 
