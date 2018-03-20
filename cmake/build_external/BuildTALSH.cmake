ae4d2510a662d73c00f4ab28f99b3453b545db3c
Ajay Panyala <ajay.panyala@gmail.com>
2018-03-20 11:16:09 -0700

    minor fix (#58)
    Former-commit-id: f6ef9e80a11c744dcb45ccc8c0d8b74f71b97693

--- cmake/build_external/BuildTALSH.cmake
+++ cmake/build_external/BuildTALSH.cmake
@@ -2,7 +2,6 @@
 
 ExternalProject_Add(TALSH_External
     GIT_REPOSITORY https://github.com/DmitryLyakh/TAL_SH.git
-    GIT_TAG cmake
     CMAKE_ARGS ${DEPENDENCY_CMAKE_OPTIONS}
     -DTALSH_GPU=ON 
     INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install DESTDIR=${STAGE_DIR}
