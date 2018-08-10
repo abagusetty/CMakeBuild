b8824827b1c3c7603ebdc2d6596fd5de5b5555a4
Ajay Panyala <ajay.panyala@gmail.com>
2018-08-09 21:46:47 -0700

    Minor update to FindGSL
    Former-commit-id: 3009e5987f5dbb64d45e704ebf943be96bb9f7dd

--- cmake/find_external/FindMSGSL.cmake
+++ cmake/find_external/FindMSGSL.cmake
@@ -1,21 +1,27 @@
 # - Try to find Microsoft GSL Library
 #
-#  To aid find_package in locating GSL, the user may set the
-#  variable GSL_ROOT_DIR to the root of the GSL install
+#  To aid find_package in locating MS GSL, the user may set the
+#  variable MSGSL_ROOT_DIR to the root of the GSL install
 #  directory.
 #
 #  Once done this will define
-#  GSL_FOUND - System has GSL
-#  GSL_INCLUDE_DIR - The GSL include directories
+#  MSGSL_FOUND - System has MS GSL
+#  MSGSL_INCLUDE_DIR - The MS GSL include directories
 
-find_path(GSL_INCLUDE_DIR gsl/gsl
-          HINTS ${PC_GSL_INCLUDEDIR}
-                ${PC_GSL_INCLUDE_DIRS}
-          PATHS ${GSL_ROOT_DIR}
+if(NOT DEFINED MSGSL_ROOT_DIR)
+    find_package(PkgConfig)
+    pkg_check_modules(PC_MSGSL QUIET gsl)
+endif()
+
+find_path(MSGSL_INCLUDE_DIR gsl/gsl
+          HINTS ${PC_MSGSL_INCLUDEDIR}
+                ${PC_MSGSL_INCLUDE_DIRS}
+          PATHS ${MSGSL_ROOT_DIR}
           )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GSL DEFAULT_MSG
-                                  GSL_INCLUDE_DIR)
+find_package_handle_standard_args(MSGSL DEFAULT_MSG
+                                  MSGSL_INCLUDE_DIR)
 
-set(GSL_INCLUDE_DIRS ${GSL_INCLUDE_DIR})
+set(MSGSL_FOUND ${MSGSL_FOUND})
+set(MSGSL_INCLUDE_DIRS ${MSGSL_INCLUDE_DIR})
