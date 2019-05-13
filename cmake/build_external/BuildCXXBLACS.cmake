2df6d5743ef7f554e148c4b3511e903990a7dfaf
Ajay Panyala <ajay.panyala@gmail.com>
2019-05-13 13:47:42 -0700

    update cxxblacs build
    Former-commit-id: 0813e19ac0a8d64295849d7d195fd712ad8ab629

--- cmake/build_external/BuildCXXBLACS.cmake
+++ cmake/build_external/BuildCXXBLACS.cmake
@@ -1,16 +1,9 @@
 
-
 ExternalProject_Add(CXXBLACS_External
-    GIT_REPOSITORY https://github.com/wavefunction91/CXXBLACS.git
+    GIT_REPOSITORY https://github.com/ajaypanyala/CXXBLACS.git
     UPDATE_DISCONNECTED 1
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND  ${CMAKE_COMMAND} -E copy
-    <SOURCE_DIR>/cxxblacs.hpp ${STAGE_DIR}/${CMAKE_INSTALL_PREFIX}/include/cxxblacs.hpp
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
-                    <SOURCE_DIR>/cxxblacs ${STAGE_DIR}/${CMAKE_INSTALL_PREFIX}/include/cxxblacs
-
-    # INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install DESTDIR=${STAGE_DIR}
-    # CMAKE_CACHE_ARGS ${CORE_CMAKE_LISTS}
-    #                  ${CORE_CMAKE_STRINGS}
+    CMAKE_ARGS ${DEPENDENCY_CMAKE_OPTIONS}
+    INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install DESTDIR=${STAGE_DIR}
+    CMAKE_CACHE_ARGS ${CORE_CMAKE_LISTS}
+                     ${CORE_CMAKE_STRINGS}
 )
-
--- cmake/find_external/FindCXXBLACS.cmake
+++ cmake/find_external/FindCXXBLACS.cmake
@@ -1,26 +1,26 @@
-# - Try to find Microsoft GSL Library
+# - Try to find CXXBLACS
 #
-#  To aid find_package in locating MS GSL, the user may set the
-#  variable CXXBLACS_ROOT_DIR to the root of the GSL install
+#  To aid find_package in locating CXXBLACS, the user may set the
+#  variable CXXBLACS_ROOT_DIR to the root of the CXXBLACS install
 #  directory.
 #
 #  Once done this will define
-#  CXXBLACS_FOUND - System has MS GSL
-#  CXXBLACS_INCLUDE_DIR - The MS GSL include directories
+#  CXXBLACS_FOUND - System has CXXBLACS
+#  CXXBLACS_INCLUDE_DIR - The CXXBLACS include directories
 
 if(NOT DEFINED CXXBLACS_ROOT_DIR)
     find_package(PkgConfig)
-    pkg_check_modules(PC_CXXBLACS QUIET CXXBLACS)
+    pkg_check_modules(PC_CXXBLACS QUIET cxxblacs)
 endif()
 
-find_path(CXXBLACS_INCLUDE_DIR cxxblacs.hpp cxxblacs/blacs.hpp
+find_path(CXXBLACS_INCLUDE_DIR cxxblacs/blacs.hpp
           HINTS ${PC_CXXBLACS_INCLUDEDIR}
                 ${PC_CXXBLACS_INCLUDE_DIRS}
           PATHS ${CXXBLACS_ROOT_DIR}
           )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MSGSL DEFAULT_MSG
+find_package_handle_standard_args(CXXBLACS DEFAULT_MSG
                                   CXXBLACS_INCLUDE_DIR)
 
 set(CXXBLACS_FOUND ${CXXBLACS_FOUND})
