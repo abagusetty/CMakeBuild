7c19295f060d1e3327f4a7caa94ee6aedd432b6b
Ajay Panyala <ajay.panyala@gmail.com>
2019-11-03 12:52:29 -0800

    switch to latest GA commit
    Former-commit-id: 22b6d683c1543b012f214c211b37ee8fec473520

--- cmake/find_external/FindScaLAPACK.cmake
+++ cmake/find_external/FindScaLAPACK.cmake
@@ -1,27 +1,26 @@
 # Find ScaLAPACK
 #
-#  Once done this will define
-#  ScaLAPACK_FOUND - System has ScaLAPACK
-#  ScaLAPACK_LIBRARIES - The ScaLAPACK include directories
+# Once done this will define
+# SCALAPACK_FOUND - System has ScaLAPACK
+# SCALAPACK_LIBRARIES - The ScaLAPACK include directories
 #
 
 include(UtilityMacros)
 include(FindPackageHandleStandardArgs)
 
-is_valid(ScaLAPACK_LIBRARIES FINDSCA_LIBS_SET)
+is_valid(SCALAPACK_LIBRARIES FINDSCA_LIBS_SET)
 if(NOT FINDSCA_LIBS_SET)
-    find_library(ScaLAPACK_LIBRARY
-        NAMES scalapack
-        HINTS ${PC_ScaLAPACK_LIBDIR} ${PC_ScaLAPACK_LIBRARY_DIRS}
-        PATHS ${ScaLAPACK_ROOT_DIR}
+    find_library(SCALAPACK_LIBRARY
+        NAMES libscalapack.a libscalapack.so mkl_scalapack_lp64 mkl_scalapack_ilp64
+        PATHS ${SCALAPACK_ROOT_DIR}
         PATH_SUFFIXES lib lib64 lib32
         DOC "ScaLAPACK Libraries"
     )
     find_package_handle_standard_args(ScaLAPACK DEFAULT_MSG
-                                      ScaLAPACK_LIBRARY)
-    set(ScaLAPACK_FOUND ${ScaLAPACK_FOUND})
-    set(ScaLAPACK_LIBRARIES ${ScaLAPACK_LIBRARY})
+                                      SCALAPACK_LIBRARY)
+    set(SCALAPACK_FOUND ${SCALAPACK_FOUND})
+    set(SCALAPACK_LIBRARIES ${SCALAPACK_LIBRARY})
 endif()
 
 find_package_handle_standard_args(ScaLAPACK DEFAULT_MSG
-                                    ScaLAPACK_LIBRARIES)
+                                  SCALAPACK_LIBRARIES)
