0664117bfae934894c29ad1ef92f4475a9e3f0b9
Ajay Panyala <ajay.panyala@gmail.com>
2022-04-29 19:43:02 -0700

    eigen: use git url
    Former-commit-id: 3eee6e63d897b37bba021ab730b26c60b7169f4d

--- cmake/build_external/BuildEigen3.cmake
+++ cmake/build_external/BuildEigen3.cmake
@@ -1,5 +1,12 @@
+set(EIGEN_GIT_TAG 0fd6b4f71dd85b2009ee4d1aeb296e2c11fc9d68) #v3.3.9
+if(ENABLE_DEV_MODE)
+    set(EIGEN_GIT_TAG master)
+endif()
+
 ExternalProject_Add(Eigen3_External
-    URL https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
+    GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+    GIT_TAG ${EIGEN_GIT_TAG}
+    UPDATE_DISCONNECTED 1
     CMAKE_ARGS ${DEPENDENCY_CMAKE_OPTIONS} 
         INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install #DESTDIR=${STAGE_DIR}
     )
