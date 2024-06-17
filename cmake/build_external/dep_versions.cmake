7798c8df5e518c958c03899f6904a760089b31ec
Ajay Panyala <ajay.panyala@gmail.com>
2024-06-17 14:45:23 -0700

    [2024-06-17] pin dep versions

--- cmake/build_external/dep_versions.cmake
+++ cmake/build_external/dep_versions.cmake
@@ -4,13 +4,13 @@ set(CMSB_HDF5_VERSION hdf5_1.14.4.3)
 set(CMSB_DOCTEST_VERSION 2.4.9)
 set(CMSB_ELPA_VERSION 2024.03.001)
 
-set(TAMM_GIT_TAG main) #2024-06-12
+set(TAMM_GIT_TAG 2024-06-17)
 if(ENABLE_DEV_MODE OR USE_TAMM_DEV)
     set(TAMM_GIT_TAG develop)
 endif()
 
 # numactl
-set(NUMACTL_GIT_TAG master)
+set(NUMACTL_GIT_TAG v2.0.18)
 
 # Eigen3
 set(EIGEN_GIT_TAG e887196d9d67e48c69168257d599371abf1c3b31) #Mar 17, 2023
@@ -58,19 +58,19 @@ if(ENABLE_DEV_MODE)
 endif()
 
 # Global Arrays
-set(GA_GIT_TAG develop)
+set(GA_GIT_TAG 6636f3ec85b9bf752f5fd9813faa11b0ddd75b7a)
 if(ENABLE_DEV_MODE)
     set(GA_GIT_TAG develop)
 endif()
 
 # HPTT
-set(HPTT_GIT_TAG master)
+set(HPTT_GIT_TAG eff1bdd79734ddc4993dd4df1d0cdbd40758b9cb)
 if(ENABLE_DEV_MODE)
     set(HPTT_GIT_TAG master)
 endif()
 
 # Librett
-set(LIBRETT_GIT_TAG master)
+set(LIBRETT_GIT_TAG 818f4f5e5a58e837cc6e71300703b65266a1039b)
 if(ENABLE_DEV_MODE)
   set(LIBRETT_GIT_TAG master)
 endif()
@@ -79,13 +79,13 @@ endif()
 set(CMSB_LIBINT_VERSION 2.9.0)
 
 # LibEcpInt
-set(ECPINT_GIT_TAG master)
+set(ECPINT_GIT_TAG ee6d75a969bb92535a9ecf2ba4b564a75b7ef84b)
 if(ENABLE_DEV_MODE)
   set(ECPINT_GIT_TAG master)
 endif()
 
 # GauXC
-set(GXC_GIT_TAG master)
+set(GXC_GIT_TAG 2c94d73e779a817dfc48a9d8629df7fdfba636d7)
 if(ENABLE_DEV_MODE)
     set(GXC_GIT_TAG master)
 endif()
